a7fa05c17d49bafb060bbb6c2cc6cd519710d44d
walterzambranom <walterzambranom@gmail.com>
2019-09-16 18:23:04 -0500

    Actualización

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { reduxForm, Field } from 'redux-form';
+import {setPropsAsInitial} from './../helpers/setPropsAsInitial';
 
 const CustomerEdit = ({ name, dni, age }) => {
 	return (
 		<div>
 			<h2>Nombre del cliente</h2>
-			<h3>Nombre: {name} / ID: {dni} / Edad: {age}</h3>
+			<form action="">
+				<div>
+					<label htmlFor="name">Nombre: </label>
+					<Field name="name" component ="input" type="text"></Field>
+				</div>
+				<div>
+					<label htmlFor="dni">Identificación: </label>
+					<Field name="dni" component ="input" type="text"></Field>
+				</div>
+				<div>
+					<label htmlFor="age">Edad: </label>
+					<Field name="age" component ="input" type="number"></Field>
+				</div>
+			</form>
 		</div>
 	);
 };
@@ -16,5 +31,6 @@ CustomerEdit.propTypes = {
 	age: PropTypes.number,
 
 };
+const CustomerEditForm = reduxForm({ form: 'CustomerEdit' })(CustomerEdit);
 
-export default CustomerEdit;
+export default setPropsAsInitial(CustomerEditForm);
